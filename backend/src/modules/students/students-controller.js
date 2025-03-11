9e546d98cf0afdb474c335121d370b88f9cbdace
pastordev <pastordev@jarvis-toolkit.com>
2025-03-11 21:41:15 +0200

    hiring test initial

--- backend/src/modules/students/students-controller.js
+++ backend/src/modules/students/students-controller.js
@@ -1,29 +1,34 @@
 const asyncHandler = require("express-async-handler");
-const { getAllStudents, addNewStudent, getStudentDetail, setStudentStatus, updateStudent } = require("./students-service");
+const { getAllStudents, addNewStudent, getStudentDetail, updateStudent, setStudentStatus } = require("./students-service");
 
 const handleGetAllStudents = asyncHandler(async (req, res) => {
-    //write your code
-
+    const payload = req.query;
+    const students = await getAllStudents(payload);
+    res.json(students);
 });
 
 const handleAddStudent = asyncHandler(async (req, res) => {
-    //write your code
-
+    const studentData = req.body;
+    const result = await addNewStudent(studentData);
+    res.json(result);
 });
 
 const handleUpdateStudent = asyncHandler(async (req, res) => {
-    //write your code
-
+    const studentData = req.body;
+    const result = await updateStudent(studentData);
+    res.json(result);
 });
 
 const handleGetStudentDetail = asyncHandler(async (req, res) => {
-    //write your code
-
+    const { id } = req.params;
+    const student = await getStudentDetail(id);
+    res.json(student);
 });
 
 const handleStudentStatus = asyncHandler(async (req, res) => {
-    //write your code
-
+    const { userId, reviewerId, status } = req.body;
+    const result = await setStudentStatus({ userId, reviewerId, status });
+    res.json(result);
 });
 
 module.exports = {
